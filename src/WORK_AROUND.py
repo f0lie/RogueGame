1334d4f01e269334cec50104045a8c971dab19e8
f0lie <zukoi.ad97@gmail.com>
2015-10-10 15:34:45 -0400

    added message on work around

--- src/WORK_AROUND.py
+++ src/WORK_AROUND.py
@@ -1,3 +1,9 @@
 import os
 
+'''
+For some reason my IDE's terminal doesn't support "redirection"
+This file is for a work around the IDE's terminal and use windows's cmd
+You don't have to run this, just run main.py and it SHOULD be fine
+'''
+
 os.system("start cmd /k python main.py")
