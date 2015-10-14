884d83ec5e2f771cfd0209fccd475bfbd4b214ae
f0lie <zukoi.ad97@gmail.com>
2015-10-14 01:11:38 -0400

    Added coords to GUI as test

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import display
 from display import Display
 import input
 import entity
+import room
 
 '''
 	Screen should be 80x24 like a VT100
@@ -18,18 +19,17 @@ def main(stdscr):
 
 	game_input = input.Input(stdscr)
 
-	game_map = map.Map(10, 10)
+	game_map = map.Map(20, 30)
 	game_display = display.DisplayMap(game_map)
 
-	game_gui = display.DisplayHook(game_display, display.Orientation.right, 12, 12)
+	first_room = room.Room(5, 5, 10, 10)
+	game_map.put_room(first_room)
 
-	for i in range(game_gui.rows - 2):
-		game_gui.print('hello', i, 0)
+	game_gui = display.DisplayHook(game_display, display.Orientation.right, 22, 20)
 
 	player = entity.Entity()
 	game_map.put_entity(player)
 
-	game_display.refresh_map()
 	game_gui.refresh()
 	Display.update()
 
@@ -41,6 +41,10 @@ def main(stdscr):
 		game_map.flush()
 
 		game_map.put_entity(player)
+
+		game_gui.print(str(player.pos.get_pos()), 0, 0, True)
+		game_gui.refresh()
+
 
 		game_display.refresh_map()
 		Display.update()
