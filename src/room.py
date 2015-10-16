16219455d52379e813ff4e9e4512d83f18f80b2f
f0lie <zukoi.ad97@gmail.com>
2015-10-15 23:44:45 -0400

    Added parameters for wall block type

--- src/room.py
+++ src/room.py
@@ -1,13 +1,28 @@
 from position import Position
+from block import Block
 
 
 class Room(object):
-	def __init__(self, pos_y=0, pos_x=0, rows=1, cols=1):
+	def __init__(self, pos_y=0, pos_x=0, rows=1, cols=1, fill=Block.empty,
+	             left=Block.wall, right=Block.wall, top=Block.wall, bottom=Block.wall,
+	             top_left=Block.wall, top_right=Block.wall, bottom_left=Block.wall, bottom_right=Block.wall):
 		self.pos_1 = Position(pos_y, pos_x)
 		self.pos_2 = Position(pos_y+rows, pos_x+cols)
 		self.rows = rows
 		self.cols = cols
+		self.fill = fill
+
+		# Specific the block of walls
+		self.left = left
+		self.right = right
+		self.top = top
+		self.bottom = bottom
+
+		self.top_left = top_left
+		self.top_right = top_right
+		self.bottom_left = bottom_left
+		self.bottom_right = bottom_right
 
 	def collision(self, other_room):
-		return (self.pos_1.x <= other_room.pos_2.x and self.pos_2.x >= other_room.pos_1.x and
-		        self.pos_1.y <= other_room.pos_2.y and self.pos_2.y >= other_room.pos_1.y)
+		return (self.pos_1.col <= other_room.pos_2.x and self.pos_2.col >= other_room.pos_1.x and
+		        self.pos_1.row <= other_room.pos_2.y and self.pos_2.row >= other_room.pos_1.y)
