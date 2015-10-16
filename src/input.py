66ac504834431f29cb9879b4eeaa17c2de85c0a4
f0lie <zukoi.ad97@gmail.com>
2015-10-15 23:42:48 -0400

    Simplified code and added comments

--- src/input.py
+++ src/input.py
@@ -1,28 +1,18 @@
-from enum import Enum
-
 from config import keys
-
-
-class Move(Enum):
-	up, down, right, left, done, none = range(6)
+from entity import Move
 
 
 class Input(object):
 	def __init__(self, scr):
 		"""
 		Uses curses screen to take in input
-
-		:param scr: Curses screen to take input
 		"""
 		self.scr = scr
 
 	def get_move_key(self):
 		input_move = chr(self.scr.getch())
 
+		# From config.py, return the correct move type
 		for key, value in keys.items():
 			if value == input_move:
-				for move_key in Move:
-					if key == move_key.name:
-						return move_key
-
-		return Move.none
+				return Move.__members__.get(key, Move.none)
