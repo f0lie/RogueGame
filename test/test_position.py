878be533c3cda04d05795cd94d12d0ae28ea0f4c
f0lie <zukoi.ad97@gmail.com>
2015-10-17 00:57:27 -0400

    Added more tests

--- test/test_position.py
+++ test/test_position.py
@@ -13,7 +13,12 @@ class TestPosition(TestCase):
 		pos_right = Position(5, 6)
 		pos_left = Position(5, 4)
 
+		pos_tests = [pos_left, pos_right, pos_bottom, pos_top]
+
 		self.assertEquals(pos.compare(pos_top), Orientation.top)
 		self.assertEquals(pos.compare(pos_bottom), Orientation.bottom)
 		self.assertEquals(pos.compare(pos_left), Orientation.left)
 		self.assertEquals(pos.compare(pos_right), Orientation.right)
+
+		for pos_case in pos_tests:
+			self.assertEquals(pos.distance(pos_case), 1)
---
+++ test/test_tunnel.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from orient import Orientation
+from position import Position
+from room import Tunnel
+
+
+class TestTunnel(TestCase):
+	def test_create_vertical(self):
+		pos_1 = Position(5, 5)
+		pos_2 = Position(6, 6)
+
+		tunnel = Tunnel.create_vertical(pos_1, pos_2)
+
+		self.assertEquals(tunnel.pos.get_pos(), pos_1.get_pos())
+		self.assertEquals(tunnel.length, 1)
+		self.assertEquals(tunnel.orient, Orientation.bottom)
+
+	def test_create_horizontal(self):
+		pos_1 = Position(5, 5)
+		pos_2 = Position(7, 7)
+
+		tunnel = Tunnel.create_horizontal(pos_1, pos_2)
+
+		self.assertEquals(tunnel.pos.get_pos(), pos_1.get_pos())
+		self.assertEquals(tunnel.length, 2)
+		self.assertEquals(tunnel.orient, Orientation.right)
