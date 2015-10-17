cd970fca4a64be9f5e3d61074d8e980705f95f87
f0lie <zukoi.ad97@gmail.com>
2015-10-16 20:28:54 -0400

    Added room generation

--- src/room.py
+++ src/room.py
@@ -1,6 +1,6 @@
 from position import Position, Size
 from block import Room, Block
-
+from random import randint
 
 class Room(object):
 	def __init__(self, pos_y=0, pos_x=0, rows=1, cols=1, fill=Block.empty,
@@ -25,9 +25,39 @@ class Room(object):
 		self.bottom_left = bottom_left
 		self.bottom_right = bottom_right
 
-	def collision(self, other_room):
-		return (self.pos_1.col <= other_room.pos_2.x and self.pos_2.col >= other_room.pos_1.x and
-		        self.pos_1.row <= other_room.pos_2.y and self.pos_2.row >= other_room.pos_1.y)
+	@classmethod
+	def from_objects(cls, pos, size, **kwargs):
+		return cls(pos.row, pos.col, size.rows, size.cols, **kwargs)
 
-	def generate(self, min_pos, max_pos, min_size, max_size):
-		pass
+	def collision(self, other_room):
+		return (self.pos_1.col <= other_room.pos_2.col and self.pos_2.col >= other_room.pos_1.col and
+		        self.pos_1.row <= other_room.pos_2.row and self.pos_2.row >= other_room.pos_1.row)
+
+	@classmethod
+	def generate(cls, min_pos, max_pos, min_size, max_size):
+		size = Size(randint(min_size.rows, max_size.rows), randint(min_size.cols, max_size.cols))
+		pos = Position(randint(min_pos.row, max_pos.row-size.rows), randint(min_pos.col, max_pos.col-size.cols))
+		return cls.from_objects(pos, size)
+
+class RoomList():
+	def __init__(self):
+		self._room_list = []
+
+	def __iter__(self):
+		return iter(self._room_list)
+
+	def append(self, room):
+		self._room_list.append(room)
+
+	def generate(self, num, min_pos, max_pos, min_size, max_size):
+		for i in range(num):
+			room = Room.generate(min_pos, max_pos, min_size, max_size)
+			while self.is_collision(room):
+				room = Room.generate(min_pos, max_pos, min_size, max_size)
+			self.append(room)
+
+	def is_collision(self, room):
+		for other_room in self._room_list:
+			if other_room.collision(room):
+				return True
+		return False
