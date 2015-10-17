c77157a7e04655723122580b0641a79937f13d64
f0lie <zukoi.ad97@gmail.com>
2015-10-16 20:29:14 -0400

    Added ability to put random rooms

--- src/map.py
+++ src/map.py
@@ -1,6 +1,7 @@
 from block import Block, Room, Entity
 from entity import Move
-
+from room import RoomList
+from position import Size, Position
 
 def bound(func):
 	"""
@@ -14,11 +15,11 @@ def bound(func):
 		if entity.pos.row < 0:
 			entity.pos.row = 0
 
-		if entity.pos.col > self.cols - 1:
-			entity.pos.col = self.cols - 1
+		if entity.pos.col > self.size.cols - 1:
+			entity.pos.col = self.size.cols - 1
 
-		if entity.pos.row > self.rows - 1:
-			entity.pos.row = self.rows - 1
+		if entity.pos.row > self.size.rows - 1:
+			entity.pos.row = self.size.rows - 1
 
 		func(self, entity)
 
@@ -49,12 +50,14 @@ def collision(func):
 
 
 class Map(object):
-	def __init__(self, rows=1, cols=1, fill=Block.space):
+	def __init__(self, rows=1, cols=1, rooms=1, room_size=Size(10,10), fill=Block.space):
 		self.map = [[fill for col in range(cols)] for row in range(rows)]
-		self.rows = rows
-		self.cols = cols
+		self.size = Size(rows, cols)
+
 		self.fill = fill
-		self.room_list = []
+		self.room_list = RoomList()
+		self.room_list.generate(rooms, Position(), Position(rows, cols), Size(5, 5), room_size)
+		self.put_room_list()
 
 	@bound
 	@collision
@@ -64,12 +67,14 @@ class Map(object):
 		"""
 		self.map[entity.pos.row][entity.pos.col] = entity.icon
 
+	def put_room_list(self):
+		for room in self.room_list:
+			self.put_room(room)
+
 	def put_room(self, room):
 		"""
-		Add room to list and place walls in the correct places
+		Add room to map and place walls in the correct places
 		"""
-		self.room_list.append(room)
-
 		for row in range(room.size.rows):
 			for col in range(room.size.cols):
 				room_row, room_col = room.pos_1.row+row, room.pos_1.col+col
@@ -104,11 +109,12 @@ class Map(object):
 		If the block isn't a wall, empty or space, then set it empty
 		Horribly inefficient as it iterates through the entire map
 		"""
-		for row in range(self.rows):
-			for col in range(self.cols):
+		for row in range(self.size.rows):
+			for col in range(self.size.cols):
 				block = self.map[row][col]
 				if (block in Entity):
 					self.set(row, col, Block.empty)
+
 	def get(self, row, col):
 		return self.map[row][col]
 
