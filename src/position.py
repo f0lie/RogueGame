36cf497e49156edea0754e621afd0f7637be3ce6
f0lie <zukoi.ad97@gmail.com>
2015-10-17 00:08:52 -0400

    Added compare and distance method

--- src/position.py
+++ src/position.py
@@ -1,3 +1,8 @@
+from math import sqrt, pow, floor, fabs
+
+from orient import Orientation
+
+
 class Position(object):
 	def __init__(self, row=0, col=0):
 		self.row = row
@@ -5,6 +10,38 @@ class Position(object):
 
 	def get_pos(self):
 		return self.row, self.col
+
+	def distance(self, other_pos):
+		y = fabs(self.row - other_pos.row)
+		x = fabs(self.col - other_pos.col)
+
+		return floor(sqrt(pow(y, 2) + pow(x, 2)))
+
+	def compare(self, other_pos):
+		"""
+		Returns the relative position of other_pos to the object
+		Thus if other_pos is to the right of object then
+		you will find other_pos to the right of object
+		"""
+		if self.row == other_pos.row:
+			if self.col < other_pos.col:
+				return Orientation.right
+			elif self.col > other_pos.col:
+				return Orientation.left
+			elif self.col == other_pos.col:
+				return Orientation.same
+
+		elif self.col == other_pos.col:
+			if self.row < other_pos.row:
+				return Orientation.bottom
+			elif self.row > other_pos.row:
+				return Orientation.top
+			elif self.row == other_pos.row:
+				return Orientation.same
+
+		else:
+			# meaning other_pos is diagonal to the object
+			return Orientation.none
 
 	def move(self, row, col):
 		self.row = row
@@ -22,10 +59,11 @@ class Position(object):
 	def move_right(self):
 		self.col += 1
 
+
 class Size(object):
 	def __init__(self, rows=1, cols=1):
 		self.rows = rows
-		self.cols =cols
+		self.cols = cols
 
 	def get_size(self):
 		return self.rows, self.cols
