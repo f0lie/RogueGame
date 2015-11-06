ec4d62cd1a0c39d02e6f74084418f37f9d450101
f0lie <alexdiep2501@gmail.com>
2015-11-06 18:27:34 -0500

    pep8 formating

--- src/tunnel.py
+++ src/tunnel.py
@@ -47,8 +47,7 @@ class Tunnel(object):
     @classmethod
     def from_pos(cls, pos_1, pos_2):
         """
-        Return a tunnel with pos_1 as its pos, the orient fro pos_1 to pos_2 as its orient, and
-        the distance between the two points as its length
+        Return a tunnel given two positions
         """
         orient = pos_1.compare(pos_2)
         if orient == Orientation.none:
@@ -60,14 +59,14 @@ class Tunnel(object):
     @classmethod
     def create_vertical(cls, pos_1, pos_2):
         """
-        From pos_1 ignore pos_2's _col to create a horizontal line to pos_2's _row
+        From pos_1's row create a horizontal tunnel to pos_2's row
         """
         return cls.from_pos(pos_1, Position(pos_2.row, pos_1.col))
 
     @classmethod
     def create_horizontal(cls, pos_1, pos_2):
         """
-        From pos_1 ignore pos_2's _row to create a vertical to pos_2's _col
+        From pos_1's col create a vertical tunnel to pos_2's col
         """
         return cls.from_pos(pos_1, Position(pos_1.row, pos_2.col))
 
@@ -82,11 +81,15 @@ class Connection(object):
 
     def _connect(self, pos_1, pos_2):
         """
-        Create two tunnels that randomly goes from horizontal to vertical or the other way to get to a point
+        Append two tunnels that go horizontal and vertical or the other way
         """
         if randint(0, 1) == 0:
             self.tunnels.append(Tunnel.create_horizontal(pos_1, pos_2))
-            self.tunnels.append(Tunnel.create_vertical(Position(pos_1.row, pos_2.col), pos_2))
+            self.tunnels.append(Tunnel.create_vertical(Position(pos_1.row,
+                                                                pos_2.col),
+                                                       pos_2))
         else:
             self.tunnels.append(Tunnel.create_vertical(pos_1, pos_2))
-            self.tunnels.append(Tunnel.create_horizontal(Position(pos_2.row, pos_1.col), pos_2))
+            self.tunnels.append(Tunnel.create_horizontal(Position(pos_2.row,
+                                                                  pos_1.col),
+                                                         pos_2))
